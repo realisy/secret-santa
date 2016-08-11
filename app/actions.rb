0c3507d307e2b2053985a10e77796326ffbedb03
Isaac Yoon <realisy@gmail.com>
2016-08-11 15:17:44 -0400

    routes branch

--- app/actions.rb
+++ app/actions.rb
@@ -3,3 +3,18 @@ get '/' do
   binding.pry
   erb :index
 end
+
+get '/register' do
+  erb :'register'
+end
+
+post '/register' do
+  
+end
+
+get ''
+
+# - Index Route;
+# - Users Routes: New, Login, Details;
+# - Events Routes: New, Enroll, Delete, Edit;
+# - Gifts Route: Add, Edit, Delete;
