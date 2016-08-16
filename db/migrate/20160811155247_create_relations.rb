f1ec50f298192da964c1ea8b3e359514eca916ce
Leonardo Rota-Rossi <leorossi@gmail.com>
2016-08-16 21:35:07 +0000

    Changes event model

--- db/migrate/20160811155247_create_relations.rb
+++ db/migrate/20160811155247_create_relations.rb
@@ -1,10 +1,10 @@
 class CreateRelations < ActiveRecord::Migration
   def change
-     add_reference :users, :city, index: true, foreign_key: true
-     add_reference :events, :city, index: true, foreign_key: true
+     add_reference :users, :city, index: true
+     add_reference :events, :city, index: true
      add_reference :events, :creator, references: :users, index: true
-     add_reference :gifts, :user, index: true, foreign_key: true
-     add_reference :invitations, :user, index: true, foreign_key: true
+     add_reference :gifts, :user, index: true
+     add_reference :invitations, :user, index: true,
      create_join_table :users, :events
   end
 end
