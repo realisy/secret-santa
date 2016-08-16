442eb1d1bda59f6a0a570351a59408d248ed4644
Leonardo Rota-Rossi <leorossi@gmail.com>
2016-08-16 21:22:56 +0000

    Changes event model

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,5 +1,5 @@
 class Event < ActiveRecord::Base
-  belongs_to :creator, class_name: "User"
+  belongs_to :creator, class_name: "User", :foreign_key => 'creator_id'
   has_and_belongs_to_many :users, join_table: "events_users"
   belongs_to :city
   has_many :targets
